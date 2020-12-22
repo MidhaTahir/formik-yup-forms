c4d7e1c489f9b8e4bc2c69627cd4f5ee8f49f0bb
MidhaTahir <midhatahirkhan2011@gmail.com>
2020-12-22 21:59:31 +0500

    formik yup forms

---
+++ src/components/YoutubeForm.js
@@ -0,0 +1,105 @@
+import React from "react";
+import { useFormik } from "formik";
+import * as Yup from "yup";
+
+const initialValues = {
+  name: "",
+  email: "",
+  channel: "",
+};
+
+const onSubmit = (values) => {
+  console.log("Form data", values);
+};
+
+const validate = (values) => {
+  //values.name , values.email , values.channel
+  let errors = {};
+  //keys of errors object should be same as values obj --> errors.name, errors.email, errors.channel
+  //errors.name = "This field is required"
+
+  //-----------------------required-----------------------------
+  if (!values.name) {
+    errors.name = "Required";
+  }
+
+  if (!values.email) {
+    errors.email = "Required";
+  } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
+    errors.email = "Invalid email format";
+  }
+
+  if (!values.channel) {
+    errors.channel = "Required";
+  }
+
+  return errors;
+};
+
+const YoutubeForm = () => {
+  //useFormik is hook which takes in object. It also returns an object
+  const formik = useFormik({
+    //initialValues keys must correspond to name attribute in form. It is like useState(“”) initially we want to assign name, email and channel empty strings
+    initialValues,
+    //onSubmit receives form state as an argument
+    onSubmit,
+    validate,
+  });
+
+  console.log("Form visited", formik.touched);
+
+  return (
+    <div className="form-control">
+      <form onSubmit={formik.handleSubmit}>
+        <div>
+          <label htmlFor="name">Name</label>
+          <input
+            type="text"
+            id="name"
+            name="name"
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
+            value={formik.values.name}
+          />
+          {formik.touched.name && formik.errors.name  ? (
+            <div className="error">{formik.errors.name}</div>
+          ) : null}
+        </div>
+
+        <div className="form-control">
+          <label htmlFor="email">Email</label>
+          <input
+            type="text"
+            id="email"
+            name="email"
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
+            value={formik.values.email}
+          />
+          {formik.touched.email && formik.errors.email ? (
+            <div className="error">{formik.errors.email}</div>
+          ) : null}
+        </div>
+
+        <div className="form-control">
+          <label htmlFor="channel">Channel</label>
+          <input
+            type="text"
+            id="channel"
+            name="channel"
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
+            value={formik.values.channel}
+          />
+          {formik.touched.channel && formik.errors.channel ? (
+            <div className="error">{formik.errors.channel}</div>
+          ) : null}
+        </div>
+
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+  );
+};
+
+export default YoutubeForm;
