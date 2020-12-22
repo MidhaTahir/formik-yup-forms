57e71399616cc5770b7eeead0233257b5988221e
MidhaTahir <midhatahirkhan2011@gmail.com>
2020-12-22 23:02:47 +0500

    getFieldProps reduces redundancy

--- src/components/oldYoutubeForm.js
+++ src/components/oldYoutubeForm.js
@@ -12,13 +12,37 @@ const onSubmit = (values) => {
   console.log("Form data", values);
 };
 
+const validate = (values) => {
+  //values.name , values.email , values.channel
+  let errors = {};
+  //keys of errors object should be same as values obj --> errors.name, errors.email, errors.channel
+  //errors.name = "This field is required"
+
+  //-----------------------required-----------------------------
+  if (!values.name) {
+    errors.name = "Required";
+  }
+
+  if (!values.email) {
+    errors.email = "Required";
+  } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
+    errors.email = "Invalid email format";
+  }
+
+  if (!values.channel) {
+    errors.channel = "Required";
+  }
+
+  return errors;
+};
+
 const validationSchema = Yup.object({
   name: Yup.string().required("Required"),
   email: Yup.string().email("Invalid email format").required("Required"),
   channel: Yup.string().required("Required"),
 });
 
-const YoutubeForm = () => {
+const oldYoutubeForm = () => {
   //useFormik is hook which takes in object. It also returns an object
   const formik = useFormik({
     //initialValues keys must correspond to name attribute in form. It is like useState(“”) initially we want to assign name, email and channel empty strings
@@ -40,7 +64,9 @@ const YoutubeForm = () => {
             type="text"
             id="name"
             name="name"
-            {...formik.getFieldProps("name")}
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
+            value={formik.values.name}
           />
           {formik.touched.name && formik.errors.name ? (
             <div className="error">{formik.errors.name}</div>
@@ -53,7 +79,9 @@ const YoutubeForm = () => {
             type="text"
             id="email"
             name="email"
-            {...formik.getFieldProps("email")}
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
+            value={formik.values.email}
           />
           {formik.touched.email && formik.errors.email ? (
             <div className="error">{formik.errors.email}</div>
@@ -66,7 +94,9 @@ const YoutubeForm = () => {
             type="text"
             id="channel"
             name="channel"
-            {...formik.getFieldProps("channel")}
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
+            value={formik.values.channel}
           />
           {formik.touched.channel && formik.errors.channel ? (
             <div className="error">{formik.errors.channel}</div>
@@ -79,4 +109,4 @@ const YoutubeForm = () => {
   );
 };
 
-export default YoutubeForm;
+export default oldYoutubeForm;
