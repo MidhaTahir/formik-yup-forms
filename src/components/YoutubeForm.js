b3661c3fda69e989c1ad8d9d6b4e45ef58fd9f7f
MidhaTahir <midhatahirkhan2011@gmail.com>
2020-12-23 23:45:12 +0500

    error message component

--- src/components/YoutubeForm.js
+++ src/components/YoutubeForm.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Formik, Form, Field, ErrorMessage } from "formik";
 import * as Yup from "yup";
+import TextError from "../components/TextError";
 
 const initialValues = {
   name: "",
@@ -18,6 +19,7 @@ const validationSchema = Yup.object({
   name: Yup.string().required("Required"),
   email: Yup.string().email("Invalid email format").required("Required"),
   channel: Yup.string().required("Required"),
+  address: Yup.string().required("Required"),
 });
 
 const YoutubeForm = () => {
@@ -31,19 +33,19 @@ const YoutubeForm = () => {
         <div className="form-control">
           <label htmlFor="name">Name</label>
           <Field type="text" id="name" name="name" />
-          <ErrorMessage name="name" />
+          <ErrorMessage name="name" component={TextError} />
         </div>
 
         <div className="form-control">
           <label htmlFor="email">Email</label>
           <Field type="text" id="email" name="email" />
-          <ErrorMessage name="email" />
+          <ErrorMessage name="email" component={TextError} />
         </div>
 
         <div className="form-control">
           <label htmlFor="channel">Channel</label>
           <Field type="text" id="channel" name="channel" />
-          <ErrorMessage name="channel" />
+          <ErrorMessage name="channel" component={TextError} />
         </div>
 
         <div className="form-control">
@@ -55,7 +57,7 @@ const YoutubeForm = () => {
           <label htmlFor="address">Address</label>
           <Field name="address">
             {(props) => {
-              const { field,form, meta } = props;
+              const { field, form, meta } = props;
               return (
                 <div>
                   <input type="text" id="address" {...field} />
@@ -64,6 +66,11 @@ const YoutubeForm = () => {
               );
             }}
           </Field>
+          <ErrorMessage name="address">
+            {(error) => {
+              return <div className="error">{error}</div>;
+            }}
+          </ErrorMessage>
         </div>
 
         <button type="submit">Submit</button>
